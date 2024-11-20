4d2e54c9c8d9894fda3b3b810c789046edfa50d5
erweber3 <92194250+erweber3@users.noreply.github.com>
2024-11-20 00:18:02 -0600

    Update webots_ros2_homework1_python.py

--- webots_ros2_homework1_python/webots_ros2_homework1_python/webots_ros2_homework1_python.py
+++ webots_ros2_homework1_python/webots_ros2_homework1_python/webots_ros2_homework1_python.py
@@ -15,6 +15,7 @@ LINEAR_VEL = 0.22
 STOP_DISTANCE = 0.2
 LIDAR_ERROR = 0.05
 LIDAR_AVOID_DISTANCE = 0.7
+LIDAR_TURN_DISTANCE = 1.0
 SAFE_STOP_DISTANCE = STOP_DISTANCE + LIDAR_ERROR
 RIGHT_SIDE_INDEX = 90
 RIGHT_FRONT_INDEX = 45 ############################### CHANGE HERE
@@ -92,16 +93,19 @@ class RandomWalk(Node):
                 return
         elif front_lidar_min < LIDAR_AVOID_DISTANCE:
             self.cmd.linear.x = 0.07 
-            if (right_lidar_min > left_lidar_min):
-                self.cmd.angular.z = -0.3
-            else:
-                self.cmd.angular.z = 0.3
+            self.cmd.angular.z = 0.3
             self.publisher_.publish(self.cmd)
             self.get_logger().info('Turning')
             self.turtlebot_moving = True
+        elif front_lidar_min < LIDAR_TURN_DISTANCE:
+            self.cmd.linear.x = 0.15
+            self.cmd.angular.z = 0.15
+            self.publisher_.publish(self.cmd)
+            self.get_logger().info('Turning_Moderate')
+            self.turtlebot_moving = True
         else:
-            self.cmd.linear.x = 0.3
-            self.cmd.linear.z = 0.0
+            self.cmd.linear.x = 0.22
+            self.cmd.linear.z = -0.2
             self.publisher_.publish(self.cmd)
             self.turtlebot_moving = True
             
